6a4c6eb44ac670a46eada705f428f447eadb2378
chetankhandait <chetankhandait16@gmail.com>
2024-03-24 22:51:48 +0530

    add images and change in the hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import Gallery from "./component/Gallery";
+// import Gallery from "./component/Gallery";
 import Hero from "./component/Hero";
 import PropertyList from "./component/PropertyList";
 import Footer from "./component/Footer";
 import Testimonial from "./component/Testinomials";
 import CustomerReviews from "./component/Testinomials";
+import ProfileCard from "./component/AboutUs";
+import ContactForm from "./component/ContactForm";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <PropertyList />
-      <Gallery />
+      {/* <Gallery /> */}
       <CustomerReviews />
+      <ProfileCard/>
+      <ContactForm/>
       <Footer />
       {/* <CarouselComponent/> */}
     </div>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,35 +10,38 @@ function Hero() {
         <section className="relative bg-[url(https://framerusercontent.com/images/uzpCtuYDgW95T4BOxvHZMRJKE.png?scale-down-to=2048)] bg-cover bg-center bg-no-repeat">
           <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
   
-          <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-            <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-              <h1 className="text-3xl font-extrabold sm:text-5xl text-left">
+          <div className="relative mx-auto  text-white font-sans max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8  ">
+            <div className="max-w-5xl text-center ltr:sm:text-left rtl:sm:text-right">
+              <h1 className="text-3xl font-sans  sm:text-8xl text-left ">
                 Let us find your
-                <strong className="block font-extrabold text-rose-700">
+                
                   {" "}
-                  Forever Home.{" "}
-                </strong>
+                 
+
+                  Confortable Home.{" "}
+                   
+                
               </h1>
   
-              <p className="mt-4 max-w-lg sm:text-xl/relaxed">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-                illo tenetur fuga ducimus numquam ea!
+              <p className="mt-4 max-w-lg text-left sm:text-xl/relaxed">
+              Your home away from home, crafted with care.
+
               </p>
   
               <div className="mt-8 flex flex-wrap gap-4 text-center">
                 <a
                   href="#"
-                  className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+                  className="block w-full rounded bg-rose-600 px-12 py-3 text-md font-semibold text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
                 >
-                  Get Started
+                Book your Hostel 
                 </a>
   
-                <a
+                {/* <a
                   href="#"
                   className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
                 >
                   Learn More
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
