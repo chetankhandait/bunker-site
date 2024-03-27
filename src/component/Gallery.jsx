859842d8c256ddda61f6c55cd204dc49dbca768a
chetankhandait <chetankhandait16@gmail.com>
2024-03-27 20:46:29 +0530

    images correction

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,226 +1,16 @@
-// function Gallery() {
-//   return (
-//     <div>
-//       <section>
-//         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-//           <header className="text-center">
-//             <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-//               New Collection
-//             </h2>
-
-//             <p className="mx-auto mt-4 max-w-md text-gray-500">
-//               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
-//               praesentium cumque iure dicta incidunt est ipsam, officia dolor
-//               fugit natus?
-//             </p>
-//           </header>
-
-//           <ul className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3">
-//             <li>
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?cs=srgb&dl=bloom-blooming-blossom-462118.jpg&fm=jpg"
-//                   alt=""
-//                   classaName="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Casual Trainers
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-
-//             <li>
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Winter Jumpers
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-
-//             <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Skinny Jeans Blue
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-//           <ul className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3">
-//             <li>
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Casual Trainers
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-
-//             <li>
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Winter Jumpers
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-
-//             <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Skinny Jeans Blue
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-//           <ul className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3">
-//             <li>
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Casual Trainers
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-
-//             <li>
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Winter Jumpers
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-
-//             <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-//               <a href="#" className="group relative block">
-//                 <img
-//                   src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
-//                   alt=""
-//                   className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-//                 />
-
-//                 <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-//                   <h3 className="text-xl font-medium text-white">
-//                     Skinny Jeans Blue
-//                   </h3>
-
-//                   <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-//                     Shop Now
-//                   </span>
-//                 </div>
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
-
-// export default Gallery;
+ 
 import React from "react";
-import Image1 from "../assets/b-1.jpg";
-import Image2 from "../assets/b-2.jpg";
-import Image3 from "../assets/b-3.jpg";
-import Image4 from "../assets/b-4.jpg";
-import Image5 from "../assets/b-5.jpg";
-import Image6 from "../assets/b-6.jpg";
-import Image7 from "../assets/b-11.jpg";
-import Image8 from "../assets/b-8.jpg";
-import Image9 from "../assets/b-9.jpg";
-import Image10 from "../assets/b-10.jpg";
-import Image11 from "../assets/IMG_1694.jpg";
+// import Image1 from "public/images/b-1.jpg";
+// import Image2 from "public/images/b-2.jpg";
+// import Image3 from "public/images/b-3.jpg";
+// import Image4 from "public/images/b-4.jpg";
+// import Image5 from "public/images/b-5.jpg";
+// import Image6 from "public/images/b-6.jpg";
+// import Image7 from "public/images/b-11.jpg";
+// import Image8 from "public/images/b-8.jpg";
+// import Image9 from "public/images/b-9.jpg";
+// import Image10 from "public/images/b-10.jpg";
+// import Image11 from "public/images/IMG_1694.jpg";
 function Gallery() {
   return (
     <div className="container mx-auto p-4">
@@ -230,24 +20,24 @@ function Gallery() {
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <div className="grid gap-4">
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image1} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-1.jpg" alt="" />
           </div>
           <div>
             <img className="h-auto max-w-full rounded-lg" src="public/images/b-14.jpg" alt="" />
           </div>
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image3} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-3.jpg" alt="" />
           </div>
         </div>
         <div className="grid gap-4">
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image2} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-2.jpg" alt="" />
           </div>
           <div>
             <img className="h-auto max-w-full rounded-lg" src="public/images/b-15.jpg" alt="" />
           </div>
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image5} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-5.jpg" alt="" />
           </div>
         </div>
         <div className="grid gap-4">
@@ -255,27 +45,27 @@ function Gallery() {
             <img className="h-auto max-w-full rounded-lg" src="public/images/b-12.jpg" alt="" />
           </div>
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image7} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-7.jpg" alt="" />
           </div>
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image8} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-8.jpg" alt="" />
           </div>
         </div>
         <div className="grid gap-4">
           <div>
-            <img className="h-auto max-w-full rounded-lg" src={Image9} alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="public/images/b-9.jpg" alt="" />
           </div>
           <div>
             <img
               className="h-auto max-w-full rounded-lg"
-              src={Image10}
+              src="public/images/b-10.jpg"
               alt=""
             />
           </div>
           <div>
             <img
               className="h-auto max-w-full rounded-lg"
-              src={Image11}
+              src="public/images/b-11.jpg"
               alt=""
             />
           </div>
