70870fa3b5322d4d006203481563c2be52cea1b1
chetankhandait <chetankhandait16@gmail.com>
2024-03-31 18:42:30 +0530

    correction in the form

--- src/component/TestComponent.jsx
+++ src/component/TestComponent.jsx
@@ -131,12 +131,12 @@ const TestComponent = () => {
           </div>
           <form className="md:col-span-8 p-10" ref={form} onSubmit={sendEmail}>
             <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <div className="w-full md: px-3 mb-6 md:mb-0">
                 <label
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-first-name"
                 >
-                  First Name
+                  Full Name
                 </label>
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -150,22 +150,7 @@ const TestComponent = () => {
                   Please fill out this field.
                 </p>
               </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="grid-last-name"
-                >
-                  Last Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-last-name"
-                  name="lastName"
-                  type="text"
-                  placeholder="Doe"
-                  required
-                />
-              </div>
+              
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
               <div className="w-full px-3">
