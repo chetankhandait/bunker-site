90f339eb32ec736247d8dd5880ca176f2326a30d
chetankhandait <chetankhandait16@gmail.com>
2024-03-31 18:44:20 +0530

    addd

--- src/component/TestComponent.jsx
+++ src/component/TestComponent.jsx
@@ -158,13 +158,13 @@ const TestComponent = () => {
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-email"
                 >
-                  Contact Number 
+                  Contact Number
                 </label>
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   id="grid-email"
                   name="from_email"
-                  type="number"
+                  type="string"
                   placeholder="+91"
                   required
                 />
